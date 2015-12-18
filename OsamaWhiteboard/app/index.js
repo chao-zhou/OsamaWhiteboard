f6e7e7472f7c122c51a39bed76c693ff4b33eac5
Administrator <admin@example.com>
2015-12-18 14:24:43 +0800

    Enable pdf and gallery

--- OsamaWhiteboard/app/index.js
+++ OsamaWhiteboard/app/index.js
@@ -1,14 +1,32 @@
-﻿(function () {
+(function () {
+    var origin = location.origin;
+    var imageToUrlThatLooksLikePdf = origin + "/app/res/test.png";
+    var urlToPdf = origin + "/app/res/test.pdf";
+
+    var pdfWidth = 595;
+    var pdfHeight = 841;
+    var pdfPageNumber = 1;
+
     var tabeebPlayer = $(".whiteboardPlugin")
           .tabeebPlayer({
-              undo: function () { console.log("undo");},
+              undo: function () { console.log("undo"); },
               redo: function () { console.log("redo"); },
-              annotationAdded: function () { console.log("annotationAdded"); },
+              annotationAdded: function () {
+                  console.log("annotationAdded");
+              },
               annotationDeleted: function () { console.log("annotationDeleted"); },
               annotationRestored: function () { console.log("annotationRestored"); },
               annotationUpdateRestored: function () { console.log("annotationUpdateRestored"); },
-              previousMedia: function () { console.log("previousMedia"); },
-              nextMedia: function () { console.log("nextMedia"); },
+              previousMedia: function () {
+                  console.log("previousMedia");
+                  pdfPageNumber = pdfPageNumber == 1 ? 1 : pdfPageNumber - 1;
+                  setPDFSlide();
+              },
+              nextMedia: function () {
+                  console.log("nextMedia");
+                  pdfPageNumber = pdfPageNumber == 27 ? 27 : pdfPageNumber + 1;
+                  setPDFSlide();
+              },
               contentDisplayed: function () { console.log("contentDisplayed"); },
               annotationsDisplayed: function () { console.log("annotationsDisplayed"); },
               loaded: function () {
@@ -16,8 +34,15 @@
                   loadPreviewImage();
               },
               error: function () { console.log("error"); },
-              galleryRequested: function () { console.log("galleryRequested"); },
-              setMedia: function () { console.log("setMedia"); },
+              galleryRequested: function () {
+                  console.log("galleryRequested");
+                  onGalleryRequested();
+              },
+              setMedia: function (event, index) {
+                  console.log("setMedia");
+                  pdfPageNumber = index + 1;
+                  setPDFSlide();
+              },
               deletePage: function () { console.log("deletePage"); },
               audioRecordingAdded: function () { console.log("audioRecordingAdded"); },
               penColorChanged: function () { console.log("penColorChanged"); },
@@ -35,25 +60,16 @@
           })
           .data("tabeebPlayer");;
 
-    var origin = location.origin;
-
-    var imageToUrlThatLooksLikePdf = origin + "/app/res/test.png";
-    var urlToPdf = origin + "/app/res/test.pdf";
-
-    var pdfWidth = 595;
-    var pdfHeight = 841;
-    var pdfPageNumber = 1;
-
     function loadPreviewImage() {
         // First set content of image, providing width and height of PDF page (so it can scale properly)
         var tabeebContent = {
             contentName: "testContent",
-            id: "testContent",
-            nextEnabled: false,
+            id: pdfPageNumber,
+            nextEnabled: true,
             previousEnabled: false,
             pdfPage: null,
             slideIndex: 0,
-            slideCount: 1,
+            slideCount: 27,
             type: TabeebContentType.Image, // type: 0
             url: imageToUrlThatLooksLikePdf,
             width: pdfWidth,
@@ -63,12 +79,44 @@
 
         //Delay 1 second to make sure player is ready.
         setTimeout(function () {
-            swtichPDF();
+            swtichPDF(1);
         }, 1000);
     }
 
     function swtichPDF() {
         // Call this when your PDF is ready.
         tabeebPlayer.swapOutImageForPdf(urlToPdf, pdfPageNumber);
+    }
+
+    function setPDFSlide() {
+        var tabeebContent = {
+            contentName: "testContent",
+            id: pdfPageNumber,
+            nextEnabled: pdfPageNumber !== 27,
+            previousEnabled: pdfPageNumber !== 1,
+            pdfPage: pdfPageNumber,
+            slideIndex: pdfPageNumber - 1,
+            slideCount: 27,
+            type: TabeebContentType.Pdf, // type: 0
+            url: urlToPdf,
+            width: pdfWidth,
+            height: pdfHeight
+        };
+        tabeebPlayer.setContent(tabeebContent);
+    }
+
+    function onGalleryRequested() {
+        var thumbnails = [];
+        for (var i = 1; i < 27; i++) {
+            thumbnails.push({
+                pageId: i,
+                pdfPage: i,
+                pdfURL: urlToPdf,
+                contentType: TabeebContentType.Pdf,
+                order: i,
+            });
+        }
+
+        tabeebPlayer.setThumbnails(thumbnails);
     }
 })();
