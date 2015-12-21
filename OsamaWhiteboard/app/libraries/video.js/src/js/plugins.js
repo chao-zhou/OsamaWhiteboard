06d4d9c4d4a8f876d5962bc09be90378ced78fb4
cody <codyb@1218team.com>
2015-12-21 08:27:54 -0800

    Added missing tabeebPlayer files.

--- OsamaWhiteboard/app/libraries/video.js/src/js/plugins.js
+++
@@ -1,9 +0,0 @@
-/**
- * the method for registering a video.js plugin
- *
- * @param  {String} name The name of the plugin
- * @param  {Function} init The function that is run when the player inits
- */
-vjs_original.plugin = function(name, init){
-  vjs_original.Player.prototype[name] = init;
-};
